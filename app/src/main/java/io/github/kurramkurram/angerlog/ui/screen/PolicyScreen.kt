2f91d8a84bba9b5c79425037952d26279a37d7a8
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2025-03-14 19:39:49 +0900

    [Add] Application Privacy Policy

--- app/src/main/java/io/github/kurramkurram/angerlog/ui/screen/PolicyScreen.kt
+++ app/src/main/java/io/github/kurramkurram/angerlog/ui/screen/PolicyScreen.kt
@@ -3,9 +3,11 @@ package io.github.kurramkurram.angerlog.ui.screen
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import io.github.kurramkurram.angerlog.R
 import io.github.kurramkurram.angerlog.ui.component.layout.AngerLogBackButtonLayout
@@ -22,7 +24,9 @@ fun PolicyScreen(modifier: Modifier = Modifier, onClickBack: () -> Unit) {
     ) {
         AndroidView(
             factory = { WebView(it) },
-            modifier = modifier.fillMaxSize(),
+            modifier = modifier
+                .fillMaxSize()
+                .padding(horizontal = 10.dp),
             update = { webView ->
                 webView.webViewClient = WebViewClient()
                 webView.loadUrl("file:///android_asset/app.html")
