1c15a3419be47410ecafbcb015f8ccdfe00557a6
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2025-03-21 12:22:14 +0900

    [Update] 初期画面を整理する

--- app/src/main/java/io/github/kurramkurram/angerlog/ui/screen/initial/InitialViewModel.kt
+++ app/src/main/java/io/github/kurramkurram/angerlog/ui/screen/initial/InitialViewModel.kt
@@ -1,7 +1,16 @@
 package io.github.kurramkurram.angerlog.ui.screen.initial
 
 import android.content.Context
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Whatshot
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Reviews
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposableTarget
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.ViewModel
+import io.github.kurramkurram.angerlog.R
 import io.github.kurramkurram.angerlog.data.repository.AgreementPolicyRepository
 
 class InitialViewModel(private val agreementPolicyRepository: AgreementPolicyRepository) :
@@ -9,4 +18,25 @@ class InitialViewModel(private val agreementPolicyRepository: AgreementPolicyRep
     fun agree(context: Context) {
         agreementPolicyRepository.agree(context)
     }
+
+    @Composable
+    fun getDetails(): List<DetailsDto> = listOf(
+        DetailsDto(
+            imageVector = Icons.Filled.Whatshot,
+            description = stringResource(R.string.initial_details_record_anger)
+        ),
+        DetailsDto(
+            imageVector = Icons.Outlined.Reviews,
+            description = stringResource(R.string.initial_details_record_look_back)
+        ),
+        DetailsDto(
+            imageVector = Icons.Outlined.Info,
+            description = stringResource(R.string.initial_details_tips)
+        ),
+    )
 }
+
+data class DetailsDto(
+    val imageVector: ImageVector,
+    val description: String,
+)
