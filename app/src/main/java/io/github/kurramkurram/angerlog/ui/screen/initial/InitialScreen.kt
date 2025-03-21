8dedd501dc6e7324ed168544522d12f3e25c4d39
GM <54581188+kurramkurram@users.noreply.github.com>
2025-03-21 13:18:37 +0900

    [Update] 初期画面を整理する (#37)

--- app/src/main/java/io/github/kurramkurram/angerlog/ui/screen/initial/InitialScreen.kt
+++ app/src/main/java/io/github/kurramkurram/angerlog/ui/screen/initial/InitialScreen.kt
@@ -1,21 +1,32 @@
 package io.github.kurramkurram.angerlog.ui.screen.initial
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import io.github.kurramkurram.angerlog.R
 import io.github.kurramkurram.angerlog.ui.component.text.AngerLogLinkText
 import kotlinx.serialization.Serializable
 import org.koin.androidx.compose.koinViewModel
-
-private const val TAG = "InitialScreen"
 
 @Serializable
 object Initial
@@ -28,18 +39,49 @@ fun InitialScreen(
     viewModel: InitialViewModel = koinViewModel(),
 ) {
     Column {
-        // 何か表示するのでColumnでネスト
         Column(
             modifier =
                 modifier
                     .weight(1f)
                     .fillMaxWidth()
+                    .fillMaxHeight()
+                    .verticalScroll(rememberScrollState())
                     .padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text(
-                modifier = modifier,
-                text = stringResource(R.string.initial_description),
+            Spacer(Modifier.height(30.dp))
+
+            Image(
+                modifier = modifier.size(50.dp),
+                painter = painterResource(R.drawable.app_icon),
+                contentDescription = stringResource(R.string.initial_app_icon_cd),
             )
+
+            Spacer(Modifier.height(10.dp))
+
+            Text(
+                text = stringResource(R.string.initial_title, stringResource(R.string.app_name)),
+                style = MaterialTheme.typography.titleLarge,
+            )
+
+            Spacer(Modifier.height(5.dp))
+
+            Text(
+                text = stringResource(R.string.initial_description),
+                style = MaterialTheme.typography.titleSmall,
+            )
+
+            Spacer(Modifier.height(20.dp))
+
+            Column(horizontalAlignment = Alignment.Start) {
+                viewModel.getDetails().forEach {
+                    InitialScreenIconListItem(
+                        modifier = modifier.padding(vertical = 5.dp),
+                        imageVector = it.imageVector,
+                        description = it.description,
+                    )
+                }
+            }
         }
 
         AngerLogLinkText(
@@ -50,15 +92,36 @@ fun InitialScreen(
             onClickLink = onClickLicense,
             suffixText = stringResource(R.string.initial_policy_suffix_text),
         )
+
+        val context = LocalContext.current
         Button(
             modifier =
                 modifier
                     .fillMaxWidth()
                     .padding(10.dp),
-            onClick = onClick,
+            onClick = {
+                onClick()
+                viewModel.agree(context)
+            },
         ) {
             Text(stringResource(R.string.initial_button_text))
-            viewModel.agree(LocalContext.current)
         }
+    }
+}
+
+@Composable
+fun InitialScreenIconListItem(
+    modifier: Modifier = Modifier,
+    imageVector: ImageVector,
+    description: String,
+) {
+    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
+        Icon(
+            modifier = modifier.padding(horizontal = 5.dp),
+            imageVector = imageVector,
+            contentDescription = description,
+        )
+
+        Text(modifier = modifier, text = description)
     }
 }
--- app/src/main/java/io/github/kurramkurram/angerlog/ui/screen/register/RegisterScreen.kt
+++ app/src/main/java/io/github/kurramkurram/angerlog/ui/screen/register/RegisterScreen.kt
@@ -40,7 +40,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
