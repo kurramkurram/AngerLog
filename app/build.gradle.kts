10f0c181c491aea9fa51cae1403bf3368a08f898
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2025-04-01 21:43:41 +0900

    [Update] version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "io.github.kurramkurram.angerlog"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
